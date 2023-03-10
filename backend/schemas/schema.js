d47cc3c883c5c069edbfc947960e86dfd772d6c1
GCMO <g.c.m.orlandi@gmail.com>
2023-03-10 11:49:57 +0100

    initial commit

---
+++ backend/schemas/schema.js
@@ -0,0 +1,18 @@
+import product from './product'
+import banner from './banner'
+
+export const schemaTypes = [
+  product, banner
+]
+
+
+// import createSchema from 'part:@sanity/base/schema-creator'
+// import schemaTypes from 'all:part:@sanity/base/schema-type'
+
+// import product from './product'
+// import banner from './banner'
+
+// export default createSchema ({
+//   name: 'default',
+//   type: schemaTypes.concat([ product, banner]),
+// }) 
