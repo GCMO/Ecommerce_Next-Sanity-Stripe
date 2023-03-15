3fd86481462d9cd76f88fe051757e544e398dd30
GCMO <g.c.m.orlandi@gmail.com>
2023-03-15 17:14:57 +0100

    updated eslintrc

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Layout } from '../components';
 import { Toaster } from 'react-hot-toast';
 import { StateContext } from '../context/StateContext';
-import '@/styles/globals.css';
+import '../styles/globals.css';
 
 export default function App({ Component, pageProps }) {
   return (
